78134f5541df188dea7850137d0d774a38ce2722
Giovanni Toraldo <me@gionn.net>
2013-01-28 21:16:17 +0100

    First commit

---
+++ pagoda/config.php
@@ -0,0 +1,10 @@
+<?php
+// database settings
+$db_host = $_SERVER["DB1_HOST"];
+$db_user = $_SERVER["DB1_USER"];
+$db_pass = $_SERVER["DB1_PASS"];
+$db_name = $_SERVER["DB1_NAME"];
+$db_prefix = "pagoda_";
+define("DB_PREFIX", "pagoda_");
+define("COOKIE_PREFIX", $_SERVER["COOKIE_PREFIX"]);
+?>
